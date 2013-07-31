d774f2565a63693173a0abf92b24d29981af8f4e
Morgan Prior <morgan@amaras-tech.co.uk>
2013-07-31 20:31:53 +0100

    Modified tag_gen

--- plugins/tag_gen.rb
+++ plugins/tag_gen.rb
@@ -1,4 +1,48 @@
 module Jekyll
+  class TagGenerator < Generator
+    safe true
+    def generate(site)
+      if site.layouts.key? 'tag_index'
+        dir = site.config['tag_dir'] || 'tag'
+        ## Create Top level tag list /tag/index.html
+        write_all_tags_index(site, dir, site.tags.keys )
+        
+        ## Create /tag/{{tag}}/index.html
+        site.tags.keys.each do |tag|
+          write_tag_index(site, File.join(dir, tag), tag)
+        end
+      end
+    end
+  
+    def write_all_tags_index(site, dir, tags)
+      index = TagsIndex.new(site, site.source, dir, tags)
+      index.render(site.layouts, site.site_payload)
+      index.write(site.dest)
+      site.pages << index
+    end
+    
+    def write_tag_index(site, dir, tag)
+      index = TagIndex.new(site, site.source, dir, tag)
+      index.render(site.layouts, site.site_payload)
+      index.write(site.dest)
+      site.pages << index
+    end
+
+  end
+
+  class TagsIndex < Page
+    def initialize(site, base, dir, tags)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = 'index.html'
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tags_index.html')
+      self.data['tags']  = tags
+      self.data['title'] = "Tags"
+    end
+  end
+  
   class TagIndex < Page
     def initialize(site, base, dir, tag)
       @site = site
@@ -14,25 +58,5 @@ module Jekyll
     end
   end
 
-  class TagGenerator < Generator
-    safe true
-    def generate(site)
-      if site.layouts.key? 'tag_index'
-        dir = site.config['tag_dir'] || 'tag'
-        site.tags.keys.each do |tag|
-          write_tag_index(site, File.join(dir, tag), tag)
-        end
-
-        puts "Think we need to add the full tag list creation Here"
-      end
-    end
-  
-    def write_tag_index(site, dir, tag)
-      index = TagIndex.new(site, site.source, dir, tag)
-      index.render(site.layouts, site.site_payload)
-      index.write(site.dest)
-      site.pages << index
-    end
-  end
 end
 
